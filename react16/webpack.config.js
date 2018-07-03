6734a269e6b0bf085633ffa28d44c8560a9e0dd3
taochang <toge0223@163.com>
2018-07-03 11:31:02 +0800

    first

--- react16/webpack.config.js
+++ react16/webpack.config.js
@@ -1,35 +1,114 @@
-const path = require('path')
-const webpack = require('webpack')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path') // 获取绝对路径用
+const webpack = require('webpack') // webpack核心
+const HtmlWebpackPlugin = require('html-webpack-plugin') // 动态生成html插件
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-    another: './src/another-module.js'
+  mode: 'development',
+  entry: [
+    'webpack-hot-middleware/client?reload=true&path=/__webpack_hmr', // webpack热更新插件，就这么写
+    './src/index.ts' // 项目入口
+  ],
+  output: {
+    path: '/', // 将打包好的文件放在此路径下，dev模式中，只会在内存中存在，不会真正的打包到此路径
+    publicPath: '/', // 文件解析路径，index.html中引用的路径会被设置为相对于此路径
+    filename: 'bundle.js' //编译后的文件名字
+  },
+  devtool: 'inline-source-map', // 报错的时候在控制台输出哪一行报错
+  context: __dirname, // entry 和 module.rules.loader 选项相对于此目录开始解析
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: ['babel-loader', 'awesome-typescript-loader'],
+      include: path.resolve(__dirname, 'src')
+    }, {
+      // 编译前通过eslint检查代码 (注释掉即可取消eslint检测)
+      test: /\.js?$/,
+      enforce: 'pre',
+      use: ['eslint-loader'],
+      include: path.resolve(__dirname, 'src')
+    }, {
+      // .js .jsx用babel解析
+      test: /\.js?$/,
+      use: ['babel-loader'],
+      include: path.resolve(__dirname, 'src')
+    }, {
+      // .css 解析
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            localIdentName: '[local]_[hash:base64:5]'
+          }
+        },
+        'postcss-loader'
+      ]
+    }, { // .less 解析 (用于解析antd的LESS文件)
+      test: /\.less$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'postcss-loader',
+        { loader: 'less-loader', options: { javascriptEnabled: true } },
+      ],
+      include: path.resolve(__dirname, 'node_modules')
+    }, {
+      // .less 解析
+      test(filePath) {
+        return /\.less$/.test(filePath) && !/\.module\.less$/.test(filePath)
+      },
+      use: [
+        'style-loader',
+        'css-loader',
+        { loader: 'less-loader', options: { javascriptEnabled: true } }
+      ],
+      include: path.resolve(__dirname, 'src')
+    }, {
+      // .less 解析
+      test: /\.module.less$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            localIdentName: '[local]_[hash:base64:5]'
+          }
+        },
+
+        { loader: 'less-loader', options: { javascriptEnabled: true } }
+      ],
+      include: path.resolve(__dirname, 'src')
+    }, {
+      // 文件解析
+      test: /\.(eot|woff|otf|svg|ttf|woff2|appcache|mp3|mp4|pdf)(\?|$)/,
+      include: path.resolve(__dirname, 'src'),
+      use: [
+        'file-loader?name=assets/[name].[ext]'
+      ]
+    }, {
+      // 图片解析
+      test: /\.(png|jpg|gif)(\?|$)/,
+      include: path.resolve(__dirname, 'src'),
+      use: [
+        'url-loader?limit=8192&name=assets/[name].[ext]'
+      ]
+    }]
   },
   plugins: [
-    new HTMLWebpackPlugin({
-      title: 'code splitting'
+    new HtmlWebpackPlugin({
+      filename: 'index.html', //生成的html存放路径，相对于 output.path
+      favicon: './public/favicon.ico', // 自动把根目录下的favicon.ico图片加入html
+      template: './public/index.html', //html模板路径
+      inject: true // 是否将js放在body的末尾
     }),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'common'
-    // })
+    new webpack.HotModuleReplacementPlugin(), // 热更新插件
+    // new CheckerPlugin()
   ],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all'
-        }
-      }
-    }
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-
-  devtool: 'cheap-module-eval-source-map'
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'] //后缀名自动补全
+  }
 }
+
